ad6b7c62c6f2cd54617fe9899a95a546dda8b011
Saptarshi Datta <saptarshidatta08@gmail.com>
2025-02-22 12:51:35 +0530

    Cart module added

--- backend-in-bazaar/src/app.module.ts
+++ backend-in-bazaar/src/app.module.ts
@@ -5,9 +5,12 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { SellerModule } from './seller/seller.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { BargainModule } from './bargain/bargain.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, ProductModule, SellerModule],
+  imports: [UserModule, CategoryModule, ProductModule, SellerModule, CartModule, OrderModule, BargainModule],
   controllers: [AppController],
   providers: [AppService],
 })
